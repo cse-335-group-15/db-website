961ba0c406d867826c5bf7c96bf80e3b112fac92
Grant Deibler <gwdeib01@louisville.edu>
2024-11-08 16:13:15 -0500

    It works! Can execute a query via the API now, putting the result in a table

--- index.js
+++ index.js
@@ -12,21 +12,37 @@ window.onload = (e) => {
 
 // API stuff
 async function sendQuery() {
-    query = queryInput.value
-    url = 'https://iu2mua5qjd.execute-api.us-east-1.amazonaws.com/prod/gwdeib01/select'
+    query = queryInput.value;
+    url = 'https://iu2mua5qjd.execute-api.us-east-1.amazonaws.com/prod/gwdeib01/select';
 
     request = {
         method: 'POST',
         body: JSON.stringify({
             query: query
         })
-    }
+    };
 
-    let response = await fetch(url, request)
-    console.log(response);
+    let response = await fetch(url, request);
+    let body = await response.json();
+
+    body.forEach((row) => {
+        addRow('queryResult', ...row)
+    });
 }
 
 
 // Table Managers
-function addRow() {}
+function addRow(tableId, ...rowData) {
+    let table = document.getElementById(tableId);
+    if (table == null) {
+        console.error(`Could not find table with id ${tableId}`)
+    }
+
+    let row = table.insertRow(-1);
+    for (let i = 0; i < rowData.length; i++) {
+        let cell = row.insertCell(i)
+        cell.appendChild(document.createTextNode(rowData[i]))
+    }
+}
+
 function clearTable() {}
